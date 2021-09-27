72b841c35637bd812bd51aabd320d72e8dc0f4fd
therealrajan <therealrajan@gmail.com>
2021-09-27 17:53:09 +0545

    Added a button to load more images and fixed some CSS styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container  = document.querySelector('.container')
 const unsplashUrl = 'https://source.unsplash.com/random/'
+const getmoreImagesButton = document.querySelector('.moreImagesButton')
+const parent = document.querySelector('.parent')
 const rows = 10
 
 for(let i = 0; i < rows * 3 ; i++){
@@ -15,3 +17,18 @@ function getRandomNum(){
 function getRandomSize(){
     return `${ getRandomNum()}x${ getRandomNum()}`
 }
+
+function getmoreImages(){
+    const newContainer = document.createElement('div')
+    newContainer.classList.add('container')
+    for(let i = 0; i < rows * 3 ; i++){
+    const img = document.createElement('img')
+    img.src = `${unsplashUrl}${getRandomSize()}`
+    newContainer.appendChild(img)
+    parent.appendChild(newContainer)
+    }
+}
+getmoreImagesButton.addEventListener('click',()=>{
+    console.log("Loading more images...")      
+    getmoreImages()
+})
